1ce32667a843d9d57ba09e664e4f1a3b0d87259f
bidtime <bidtime@126.com>
2018-04-12 15:46:04 +0800

    add redisCacheTransfer

---
+++ src/main/java/org/mybatis/caches/redis/RedisCacheTransfer.java
@@ -0,0 +1,13 @@
+package org.mybatis.caches.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+public class RedisCacheTransfer {
+
+  @Autowired
+  public void setRedisConnectionFactory(RedisConnectionFactory factory) {
+    RedisCaches.setRedisConnectionFactory(factory);
+  }
+
+}
